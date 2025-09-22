f8530246e8ecb76284bc8484cf5c0fae461e0160
GLakshmiJahnavi92 <ljahnavi92@gmail.com>
2025-09-22 10:31:27 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 // Testing Jenkins-webhook
 // Testing GitHub Webhook trigger
-//slavenode
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello DevOps from Java!");
     }
 }
+
 
 
 
